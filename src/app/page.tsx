c3cd3fb4d528917c9faf34135cead29dededba8c
masud90895 <masudhossainmbs129@gmail.com>
2024-01-18 19:36:33 +0600

    Remove unused middleware and update Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import { isLoggedIn } from "@/helpers/auth.service";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   const userLoggedIn = isLoggedIn();
-  const router = useRouter();
+  console.log("🚀 ~ Home ~ userLoggedIn:", userLoggedIn);
+  // const router = useRouter();
 
-  if (userLoggedIn || typeof window === "undefined") {
-    router.push("/dashboard");
-  } else {
-    router.push("/sign-in");
-  }
+  // if (userLoggedIn && typeof window !== "undefined") {
+  //   return router.push("/dashboard");
+  // } else if (!userLoggedIn) {
+  //   return router.push("/sign-in");
+  // }
 
   return <main>Home</main>;
 }
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { tokenKey } from "./helpers/token/tokenKey";
+
+const protectedRoutes = ["/dashboard", "/dashboard/users"];
+
+export default function middleware(req: NextRequest) {
+  // if (typeof window !== "undefined") {
+  //   const isUserLogin = localStorage.getItem(tokenKey);
+  //   if (!isUserLogin && protectedRoutes.includes(req.nextUrl.pathname)) {
+  //     const absoluteURL = new URL("/sign-in", req.nextUrl.origin);
+  //     return NextResponse.redirect(absoluteURL.toString());
+  //   }
+  // }
+}
